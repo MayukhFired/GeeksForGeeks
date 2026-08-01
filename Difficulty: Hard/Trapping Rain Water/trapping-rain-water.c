f995974662d145c749949f7f723f108cf0bf52ef
Mayukh Ghosh <ghoshmayukh175@gmail.com>
2026-08-01 18:24:17 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Trapping Rain Water/trapping-rain-water.c
@@ -0,0 +1,27 @@
+int maxWater(int* arr, int n) {
+    // code here
+    int total_water = 0;
+    int left = 0;
+    int right = n - 1;
+    int left_max = 0;
+    int right_max = 0;
+    while(left < right){
+        if(arr[left] < arr[right]){
+            if(arr[left] >= left_max){
+                left_max = arr[left];
+            }else{
+                total_water += left_max - arr[left];
+            }
+            left++;
+        }else{
+            if(arr[right] >= right_max){
+                right_max = arr[right];
+            }else{
+                total_water += right_max - arr[right];
+            }
+            right--;
+        }
+    }
+    
+    return total_water;
+}
